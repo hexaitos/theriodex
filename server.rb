d541256a58bed6b2815f329f9b2dfaf6cc54f12b
Hexaitos <me@hexaitos.com>
2025-08-18 17:12:13 +0200

    Work on CSS. Add back sprite in addition to front.

--- server.rb
+++ server.rb
@@ -13,10 +13,19 @@ get "/" do
     db = SQLite3::Database.new "db.sqlite3"
 
     pokemon_name = db.execute("select name from pokemon_v2_pokemon where pokemon_species_id = #{selected_pokemon};").first.first.to_s.capitalize
-    pokemon_sprite = JSON.parse(db.execute("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{selected_pokemon};").first.first.to_s)["front_default"]
+    pokemon_sprite = JSON.parse(db.execute("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{selected_pokemon};").first.first.to_s)["front_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
+    begin
+        pokemon_sprite_back = JSON.parse(db.execute("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{selected_pokemon};").first.first.to_s)["back_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
+    rescue NoMethodError
+      pokemon_sprite_back = nil
+    end
     db.execute("select name from pokemon_v2_movename where language_id = 9 and move_id in (select distinct move_id from pokemon_v2_pokemonmove where pokemon_id = #{selected_pokemon});").each do | move |
       pokemon_moves << "#{move.first.to_s}<br/>"
     end
 
-    erb :index, locals: {:sprite => pokemon_sprite, :name => pokemon_name, :moves => pokemon_moves}
+    if pokemon_sprite_back == nil then
+      puts "OWLIE"
+    end
+
+    erb :index, locals: {:sprite => pokemon_sprite, :name => pokemon_name, :moves => pokemon_moves, :sprite_back => pokemon_sprite_back}
 end
