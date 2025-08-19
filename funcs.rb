043fa85dd48208d7faee4f9b62b3e2e5bc0dce1c
Hexaitos <me@hexaitos.com>
2025-08-19 20:11:52 +0200

    Remove old, unused functions

--- funcs.rb
+++ funcs.rb
@@ -1,12 +1,3 @@
-def get_pokemon_data(host, pokemon)
-    uri = URI("http://#{host}/api/v2/pokemon/#{pokemon}/")
-    res = Net::HTTP.get_response(uri)
-    return res.body if res.is_a?(Net::HTTPSuccess)
-end
-
-def parse_json_data(json)
-end
-
 def get_pokemon_info(pokemon_id)
     pokemon_data = {}
 
