685f68529ed5e9ec4f13e3af8e386a65373092de
Hexaitos <me@hexaitos.com>
2025-08-22 13:56:01 +0200

    Complete restructuring of funcs.rb, everything in its own function, call DB only once

--- funcs.rb
+++ funcs.rb
@@ -6,98 +6,104 @@ end
 
 # TODO instead of opening the every time, maybe open the DB ONCE as soon as the server starts and then just get everything from the variables? Would probably make it quite a bit faster I am imagining
 
-def get_pokemon_info(pokemon_id)
-    pokemon_data = {}
-    pokemon_data[:evolutions] = []
-
-    db = SQLite3::Database.new "db.sqlite3"
-
-    pokemon_data[:name] = format_pokemon_name(db.get_first_value("select name from pokemon_v2_pokemon where pokemon_species_id = #{pokemon_id};").to_s)
-    pokemon_data[:sprite] = JSON.parse(db.get_first_value("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").to_s)["front_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
-    begin
-        pokemon_data[:sprite_back] = JSON.parse(db.get_first_value("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").to_s)["back_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
-    rescue NoMethodError
-        pokemon_data[:sprite_back] = nil
-    end
-
-    pokemon_data[:types] = db.execute("select type_id from pokemon_v2_pokemontype where pokemon_id = #{pokemon_id};")
-
-    pokemon_data[:flavour_text] = db.get_first_value("select flavor_text from pokemon_v2_pokemonspeciesflavortext where pokemon_species_id = #{pokemon_id} and language_id = 9 order by random() limit 1;").to_s.gsub("", " ")
-
-    pokemon_data[:species_name] = db.get_first_value("select genus from pokemon_v2_pokemonspeciesname where language_id = 9 and pokemon_species_id = #{pokemon_id};").to_s
-
-    pokemon_data[:weight] = db.get_first_value("select weight from pokemon_v2_pokemon where pokemon_species_id = #{pokemon_id};").to_f
-
-    pokemon_data[:height] = db.get_first_value("select height from pokemon_v2_pokemon where pokemon_species_id = #{pokemon_id};").to_f
-
-    db.execute("select ps2.name from pokemon_v2_pokemonspecies ps2 join pokemon_v2_pokemonspecies ps on ps2.evolution_chain_id = ps.evolution_chain_id join pokemon_v2_pokemon p on p.name = ps.name where p.pokemon_species_id = #{pokemon_id};").each do |form|
-     pokemon_data[:evolutions] << form.first.to_s
-    end
-
-    return pokemon_data
-end
-
-# TODO consolidate some stuff so that get_pokemon_info_by_name and get_pokemon_info aren't essentially the same but with just one minor difference
 # TODO all the gsub and stuff… maybe I can make a method that formats stuff how I want it to instead
+
+DB = SQLite3::Database.new "db.sqlite3"
 
 def get_pokemon_info_by_name(pokemon_name)
     pokemon_data = {}
     pokemon_data[:evolutions] = []
+    pokemon_id = DB.get_first_value("select id from pokemon_v2_pokemon where name = '#{pokemon_name}';").to_i
+    pokemon_data = get_pokemon_info(pokemon_id)
+    pokemon_data[:id] = pokemon_id
+    return pokemon_data
 
-    db = SQLite3::Database.new "db.sqlite3"
+end
 
-    pokemon_data[:id] = db.get_first_value("select id from pokemon_v2_pokemon where name = '#{pokemon_name}';").to_i
-    pokemon_id = pokemon_data[:id]
+def get_pokemon_info(pokemon_id)
+    pokemon_data = {}
 
-    pokemon_data[:name] = format_pokemon_name(db.get_first_value("select name from pokemon_v2_pokemon where pokemon_species_id = #{pokemon_id};").to_s)
-    pokemon_data[:sprite] = JSON.parse(db.get_first_value("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").to_s)["front_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
-    begin
-        pokemon_data[:sprite_back] = JSON.parse(db.get_first_value("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").to_s)["back_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
-    rescue NoMethodError
-        pokemon_data[:sprite_back] = nil
-    end
-
-    pokemon_data[:types] = db.execute("select type_id from pokemon_v2_pokemontype where pokemon_id = #{pokemon_id};")
-
-    pokemon_data[:flavour_text] = db.get_first_value("select flavor_text from pokemon_v2_pokemonspeciesflavortext where pokemon_species_id = #{pokemon_id} and language_id = 9 order by random() limit 1;").to_s.gsub("", " ")
-
-    pokemon_data[:species_name] = db.get_first_value("select genus from pokemon_v2_pokemonspeciesname where language_id = 9 and pokemon_species_id = #{pokemon_id};").to_s
-
-    pokemon_data[:weight] = db.get_first_value("select weight from pokemon_v2_pokemon where pokemon_species_id = #{pokemon_id};").to_f
-
-    pokemon_data[:height] = db.get_first_value("select height from pokemon_v2_pokemon where pokemon_species_id = #{pokemon_id};").to_f
-
-    db.execute("select ps2.name from pokemon_v2_pokemonspecies ps2 join pokemon_v2_pokemonspecies ps on ps2.evolution_chain_id = ps.evolution_chain_id join pokemon_v2_pokemon p on p.name = ps.name where p.pokemon_species_id = #{pokemon_id};").each do |form|
-     pokemon_data[:evolutions] << form.first.to_s
-    end
+    pokemon_data[:types] = get_pokemon_types(pokemon_id)
+    pokemon_data[:flavour_text] = get_pokemon_flavour_text(pokemon_id)
+    pokemon_data[:species_name] = get_pokemon_genus(pokemon_id)
+    pokemon_data[:weight] = get_pokemon_weight(pokemon_id)
+    pokemon_data[:height] = get_pokemon_height(pokemon_id)
+    pokemon_data[:evolutions] = get_pokemon_evolutions(pokemon_id)
+    pokemon_data[:name] = get_pokemon_name(pokemon_id)
+    pokemon_data[:sprite] = get_pokemon_front_sprite(pokemon_id)
+    pokemon_data[:sprite_back] = get_pokemon_back_sprite(pokemon_id)
 
     return pokemon_data
 end
 
+def get_pokemon_name(pokemon_id)
+    return format_pokemon_name(DB.get_first_value("select name from pokemon_v2_pokemon where pokemon_species_id = #{pokemon_id};").to_s)
+end
+
+def get_pokemon_front_sprite(pokemon_id)
+    return JSON.parse(DB.get_first_value("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").to_s)["front_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
+end
+
+def get_pokemon_back_sprite(pokemon_id)
+    begin
+        return JSON.parse(DB.get_first_value("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").to_s)["back_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
+    rescue NoMethodError
+        return nil
+    end
+end
+
+def get_pokemon_evolutions(pokemon_id)
+    evolutions = []
+
+    DB.execute("select ps2.name from pokemon_v2_pokemonspecies ps2 join pokemon_v2_pokemonspecies ps on ps2.evolution_chain_id = ps.evolution_chain_id join pokemon_v2_pokemon p on p.name = ps.name where p.pokemon_species_id = #{pokemon_id};").each do |form|
+     evolutions << form.first.to_s
+    end
+
+    return evolutions
+end
+
+def get_pokemon_height(pokemon_id)
+    DB.get_first_value("select height from pokemon_v2_pokemon where pokemon_species_id = #{pokemon_id};").to_f
+end
+
+def get_pokemon_weight(pokemon_id)
+    DB.get_first_value("select weight from pokemon_v2_pokemon where pokemon_species_id = #{pokemon_id};").to_f
+end
+
+def get_pokemon_types(pokemon_id)
+    return DB.execute("select type_id from pokemon_v2_pokemontype where pokemon_id = #{pokemon_id};")
+end
+
+def get_pokemon_flavour_text(pokemon_id)
+    return DB.get_first_value("select flavor_text from pokemon_v2_pokemonspeciesflavortext where pokemon_species_id = #{pokemon_id} and language_id = 9 order by random() limit 1;").to_s.gsub("", " ")
+end
+
+def get_pokemon_genus(pokemon_id)
+    return DB.get_first_value("select genus from pokemon_v2_pokemonspeciesname where language_id = 9 and pokemon_species_id = #{pokemon_id};").to_s
+end
+
 def damage_taken(types)
-    db = SQLite3::Database.new "db.sqlite3"
     pokemon_damage_taken = []
 
     if types.size == 1 then
-         pokemon_damage_taken = db.execute("select damage_type_id, case when count(*) = 1 then max(damage_factor) when min(damage_factor) = 0 then 0 else round((min(damage_factor) * max(damage_factor)) / 100.0) end as combined_damage_factor from pokemon_v2_typeefficacy where target_type_id in (#{types[0].first}) group by damage_type_id order by damage_type_id asc;")
+         pokemon_damage_taken = DB.execute("select damage_type_id, case when count(*) = 1 then max(damage_factor) when min(damage_factor) = 0 then 0 else round((min(damage_factor) * max(damage_factor)) / 100.0) end as combined_damage_factor from pokemon_v2_typeefficacy where target_type_id in (#{types[0].first}) group by damage_type_id order by damage_type_id asc;")
     else
-         pokemon_damage_taken = db.execute("select damage_type_id, case when count(*) = 1 then max(damage_factor) when min(damage_factor) = 0 then 0 else round((min(damage_factor) * max(damage_factor)) / 100.0) end as combined_damage_factor from pokemon_v2_typeefficacy where target_type_id in (#{types[0].first}, #{types[1].first}) group by damage_type_id order by damage_type_id asc;")
+         pokemon_damage_taken = DB.execute("select damage_type_id, case when count(*) = 1 then max(damage_factor) when min(damage_factor) = 0 then 0 else round((min(damage_factor) * max(damage_factor)) / 100.0) end as combined_damage_factor from pokemon_v2_typeefficacy where target_type_id in (#{types[0].first}, #{types[1].first}) group by damage_type_id order by damage_type_id asc;")
     end
 
     return pokemon_damage_taken
 end
 
 def search_for_pokemon(query)
-    db = SQLite3::Database.new "db.sqlite3"
     search_results = ""
-    pokemon_names = db.execute("select name from pokemon_v2_pokemon;")
-    pokemon_forms = db.execute("select id, name from pokemon_v2_pokemonform where form_name <> '';")
+    pokemon_names = DB.execute("select name from pokemon_v2_pokemon;")
+    pokemon_forms = DB.execute("select id, name from pokemon_v2_pokemonform where form_name <> '';")
     matches = FuzzyMatch.new(pokemon_names, :find_all_with_score =>true).find(query)
 
     matches.each do |key,value|
 
       # TODO this is all just kinda garbage omg I need to make this not terrible
-      pokemon_id = db.get_first_value("select pokemon_species_id from pokemon_v2_pokemon where name = '#{key.first.to_s}';").to_s
+      pokemon_id = DB.get_first_value("select pokemon_species_id from pokemon_v2_pokemon where name = '#{key.first.to_s}';").to_s
 
       if pokemon_forms.map(&:last).include?(key.first.to_s) then
         id_by_form = pokemon_forms.map(&:reverse).to_h
@@ -105,14 +111,14 @@ def search_for_pokemon(query)
         puts key.first.to_s
 
         begin
-            puts pokemon_sprite = JSON.parse(db.execute("select sprites from pokemon_v2_pokemonformsprites where pokemon_form_id = #{id};
+            puts pokemon_sprite = JSON.parse(DB.execute("select sprites from pokemon_v2_pokemonformsprites where pokemon_form_id = #{id};
 ").first.first.to_s)["front_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
         rescue
-            pokemon_sprite = JSON.parse(db.execute("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").first.first.to_s)["front_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
+            pokemon_sprite = JSON.parse(DB.execute("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").first.first.to_s)["front_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
         end
       else
 
-        pokemon_sprite = JSON.parse(db.execute("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").first.first.to_s)["front_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
+        pokemon_sprite = JSON.parse(DB.execute("select sprites from pokemon_v2_pokemonsprites where pokemon_id = #{pokemon_id};").first.first.to_s)["front_default"].gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "")
       end
 
       search_results << "<a href='/show/#{pokemon_id}'><img src='#{pokemon_sprite}'/><br/>#{format_pokemon_name(key.first.to_s)}<br/></a>" if value >= 0.25
