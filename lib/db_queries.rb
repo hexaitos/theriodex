80b6f7b1b21e7833d5dbc34d3bedd1295ea84998
Hexaitos <me@hexaitos.com>
2025-08-29 16:52:12 +0200

    toggle between sprites will not change language

--- lib/db_queries.rb
+++ lib/db_queries.rb
@@ -6,9 +6,9 @@ end
 
 DB = SQLite3::Database.new "db.sqlite3"
 
-def get_pokemon_info_by_name(pokemon_name)
+def get_pokemon_info_by_name(pokemon_name, language_id=9)
 	pokemon_id = DB.get_first_value("select id from pokemon_v2_pokemon where name = ?", pokemon_name).to_i
-	pokemon_data = get_pokemon_info(pokemon_id)
+	pokemon_data = get_pokemon_info(pokemon_id, language_id)
 	return pokemon_data
 end
 
