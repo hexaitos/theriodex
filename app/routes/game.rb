5adee8543e8e05613ab6649e365823e8306bda9e
Hexaitos <me@hexaitos.com>
2025-09-07 14:39:40 +0200

    make game more better

--- app/routes/game.rb
+++ app/routes/game.rb
@@ -1,17 +1,24 @@
 get "/game" do
 	random_pokemon = rand(1..1024)
 	pokemon_info = pokemon_view_index(random_pokemon)
-	session[:name] = pokemon_info[:name]
+	session[:pokemon_info] = pokemon_info
+	session[:points] ||= 0
+	pokemon_info[:points] = session[:points]
 
-	erb :game, locals: pokemon_view_index(random_pokemon)
+	erb :game, locals: pokemon_info
 end
 
 post "/game/guess" do
-	puts session[:name].downcase
-	puts params['guess'].downcase
-	if session[:name].downcase == params['guess'].downcase then
-		'GOOD'
+	if session[:pokemon_info][:name].downcase == params['guess'].downcase then
+		correct = true
+		session[:points] += 1
 	else
-		'BAD'
+		correct = false
 	end
+
+	erb :guess, locals: { :pokemon_info => session[:pokemon_info], :guess => params['guess'], :correct => correct }
+end
+
+get "/game/clear" do
+	session.clear
 end
