789e9eeb4815be471f48feeae619c0798f2887d9
Hexaitos <me@hexaitos.com>
2025-09-08 15:59:36 +0200

    add some hints

--- app/db/get_game_info.rb
+++ app/db/get_game_info.rb
@@ -1,8 +1,15 @@
 def get_game_info(id, language_id=9)
+	name = get_pokemon_name(id, language_id)
+	hints = {
+				:first_letter => name[0].upcase,
+				:last_letter => name[name.length - 1].upcase
+			}
+
 	return 	{
 				:id => id,
-				:name => get_pokemon_name(id, language_id),
+				:name => name,
 				:lang => LANGUAGE_CODES.key(language_id),
-				:sprite => get_pokemon_sprites(id)[:front_sprite]
+				:sprite => get_pokemon_sprites(id)[:front_sprite],
+				:hints => hints
 			}
 end
