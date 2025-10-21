c6f3c6daf7fb03d8ca076c0ddc7a4e4e6ad71085
Hexaitos <me@hexaitos.com>
2025-10-21 23:31:02 +0200

    linting

--- app/db/get_pokemon_type_name.rb
+++ app/db/get_pokemon_type_name.rb
@@ -1,3 +1,3 @@
 def get_pokemon_type_name(type, language_id)
-	DB.get_first_value("select name from pokemon_v2_typename where type_id = ? and language_id = ?;", [type, language_id])
+	DB.get_first_value("select name from pokemon_v2_typename where type_id = ? and language_id = ?;", [ type, language_id ])
 end
