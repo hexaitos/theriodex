b21621b92ee2eb69702f1002346dac9fd08555f1
Hexaitos <me@hexaitos.com>
2025-10-23 17:45:33 +0200

    fall back to English genus, flavour text and generation

--- app/db/get_pokemon_flavour_text.rb
+++ app/db/get_pokemon_flavour_text.rb
@@ -1,3 +1,7 @@
 def get_pokemon_flavour_text(pokemon_id, language_id = 9)
-	DB.get_first_value("select flavor_text from pokemon_v2_pokemonspeciesflavortext where pokemon_species_id = ? and language_id = ? order by random() limit 1;", [ pokemon_id, language_id ]).to_s.gsub("", " ").gsub("\n", " ").gsub("  ", "")
+	flavour_text = DB.get_first_value("select flavor_text from pokemon_v2_pokemonspeciesflavortext where pokemon_species_id = ? and language_id = ? order by random() limit 1;", [ pokemon_id, language_id ]).to_s.gsub("", " ").gsub("\n", " ").gsub("  ", "")
+
+	flavour_text = get_pokemon_flavour_text(pokemon_id, 9) if flavour_text.nil? or flavour_text.length == 0
+
+	flavour_text
 end
--- app/db/get_pokemon_genus.rb
+++ app/db/get_pokemon_genus.rb
@@ -1,3 +1,7 @@
 def get_pokemon_genus(pokemon_id, language_id = 9)
-	DB.get_first_value("select genus from pokemon_v2_pokemonspeciesname where language_id = ? and pokemon_species_id = ?;", [ language_id, pokemon_id ]).to_s
+	genus = DB.get_first_value("select genus from pokemon_v2_pokemonspeciesname where language_id = ? and pokemon_species_id = ?;", [ language_id, pokemon_id ]).to_s
+
+	genus = get_pokemon_genus(pokemon_id, 9) if genus.nil? or genus.length == 0
+
+	return genus
 end
