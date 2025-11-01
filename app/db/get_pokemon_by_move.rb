787d190c92d1c55f67820dd0b4af5d5f2d36190a
Hexaitos <me@hexaitos.com>
2025-11-01 18:17:16 +0100

    work on showing pokemon that learn a particular move in a particular gen

--- app/db/get_pokemon_by_move.rb
+++ app/db/get_pokemon_by_move.rb
@@ -1,17 +1,22 @@
 def get_pokemon_by_move(move_id, generation_id, language_id = 9)
-	DB.execute(<<-SQL, [ language_id, language_id, language_id, language_id, language_id, generation_id, move_id ])
-		SELECT
+	DB.results_as_hash = true
+	pokemon = DB.execute(<<-SQL, [ language_id, language_id, language_id, language_id, language_id, language_id, generation_id, move_id ])
+		SELECT DISTINCT
 			pokemonmove.pokemon_id,
 			name.name,
 			move.type_id,
 			typename.name AS type_name,
 			move.move_damage_class_id,
 			vg.generation_id,
-			genname.name,
+			genname.name as generation_name,
 			pokemonmove.move_id,
 			pokemonmove.move_learn_method_id,
 			learnmethodname.name AS learn_method_name,
-			vn.name AS version_name
+			vn.name AS version_name,
+			sprites.sprites,
+			vg.name as version_db,
+			gen.name as gen_db,
+			pokemon_species.name as pokemon_name
 		FROM pokemon_v2_pokemonmove pokemonmove
 		JOIN pokemon_v2_move move
 			ON move.id = pokemonmove.move_id
@@ -31,9 +36,16 @@ def get_pokemon_by_move(move_id, generation_id, language_id = 9)
 		JOIN pokemon_v2_generationname genname
 			ON genname.generation_id = vg.generation_id
 			AND genname.language_id = ?
+		JOIN pokemon_v2_generation gen
+			ON gen.id = genname.generation_id
 		JOIN pokemon_v2_typename typename
 			ON typename.type_id = move.type_id
 			AND typename.language_id = ?
+		JOIN pokemon_v2_pokemonsprites sprites
+			ON sprites.pokemon_id = pokemonmove.pokemon_id
+		JOIN pokemon_v2_pokemonspeciesname pokemon_species
+			ON pokemon_species.pokemon_species_id = pokemonmove.pokemon_id
+			AND pokemon_species.language_id = ?
 		WHERE
 			vg.generation_id = ?
 			AND pokemonmove.move_id = ?
@@ -44,4 +56,7 @@ def get_pokemon_by_move(move_id, generation_id, language_id = 9)
 			pokemonmove.pokemon_id,
 			name.name ASC;
 	SQL
+	DB.results_as_hash = false
+
+	pokemon
 end
--- app/helpers/partials.rb
+++ app/helpers/partials.rb
@@ -2,4 +2,18 @@ helpers do
 	def partial(name, locals = {})
 		erb :"partials/#{name}", layout: false, locals: locals
 	end
+
+	def pokemon_sprite_url(entry)
+	begin
+		sprites = JSON.parse(entry["sprites"] || "{}")
+
+		url = sprites.dig("versions", entry["gen_db"], entry["version_db"], "front_transparent") ||
+		sprites.dig("versions", entry["gen_db"], entry["version_db"], "front_default") ||
+		sprites["front_default"]
+
+		url&.gsub("https://raw.githubusercontent.com/PokeAPI/sprites/master", "") || ""
+	rescue
+		""
+	end
+end
 end
