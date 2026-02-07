b5b6c3775a98231d14d58f183eb53327c4b2bcab
Hexaitos <me@hexaitos.com>
2026-02-07 20:23:23 +0100

    new font!

--- app/helpers/convert_damage.rb
+++ app/helpers/convert_damage.rb
@@ -1,11 +1,11 @@
 def convert_damage(damage)
 	if damage.to_f / 100 == 0.5 then
-		 converted_damage = "½"
+		 converted_damage = "<sup>1</sup>&frasl;<sub>2</sub>"
 	elsif damage.to_f / 100 == 0.25 then
-		 converted_damage = "¼"
+		 converted_damage = "<sup>1</sup>&frasl;<sub>4</sub>"
 	else
 		converted_damage = damage.to_f / 100
-		converted_damage = converted_damage.to_s.gsub(/(\.)0+$/, '')
+		converted_damage = converted_damage.to_s.gsub(/(\.)0+$/, "")
 	end
 
 	converted_damage
