89a533717c7f2824f1dc4cc47de71a13ebec71b6
Hexaitos <me@hexaitos.com>
2026-02-28 21:15:56 +0100

    always get english gen names for now

--- app/db/get_pokemon_generation.rb
+++ app/db/get_pokemon_generation.rb
@@ -1,4 +1,6 @@
 def get_pokemon_generation(id, lang = 9)
+	lang = 9
+
 	is_available = DB.get_first_value("SELECT * FROM pokemon_v2_generationname WHERE generation_id IN (SELECT generation_id FROM pokemon_v2_pokemonspecies WHERE id = ?) UNION ALL SELECT * FROM pokemon_v2_generationname WHERE generation_id IN (SELECT generation_id FROM pokemon_v2_pokemonformgeneration WHERE pokemon_form_id = ?) LIMIT 1", id, id)
 
 	generation_info = DB.execute("select name, generation_id from pokemon_v2_generationname where generation_id in (select generation_id from pokemon_v2_pokemonspecies where id = ?) and language_id=?;", [ id, lang ]).first if id <= 1024
