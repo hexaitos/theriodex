2bc2ea025a1257133c5566ead0e0c313a28232c1
Hexaitos <me@hexaitos.com>
2026-03-02 15:10:50 +0100

    add JSON endpoints

--- app/routes/main.rb
+++ app/routes/main.rb
@@ -4,3 +4,16 @@ get "/" do
 
 	erb :index, locals: pokemon_view_index(random_pokemon, params[:form], params[:s], params[:animated], lang)
 end
+
+get "/random/json" do
+	random_pokemon = rand(1..1024)
+	puts lang =  LANGUAGE_CODES.has_key?(params[:lang].to_s.downcase) ? LANGUAGE_CODES[params[:lang].to_s.downcase] : "en"
+
+	pokemon_view_index(random_pokemon, params[:form], params[:s], params[:animated], lang).to_json
+end
+
+get "/:id/json" do
+	lang = LANGUAGE_CODES.has_key?(params[:lang].to_s.downcase) ? LANGUAGE_CODES[params[:lang].to_s.downcase] : "en"
+
+	pokemon_view_index(params[:id], params[:form], params[:s], params[:animated], lang).to_json
+end
